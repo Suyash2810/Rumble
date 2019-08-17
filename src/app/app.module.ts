deba62e785252ea607a87b708b2e063cf893cda0
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-08-17 19:46:09 +0530

    Created the basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// tslint:disable-next-line: quotemark
+import { BrowserModule } from "@angular/platform-browser";
+// tslint:disable-next-line: quotemark
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+// tslint:disable-next-line: quotemark
+import { AppComponent } from "./app.component";
+// tslint:disable-next-line: quotemark
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PostCreateComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
+})
+export class PostCreateComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+  onAddPostSubmit() {
+    alert("The post has been clicked.");
+  }
+}
