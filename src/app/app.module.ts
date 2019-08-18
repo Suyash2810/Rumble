5de7f366d9be38b90f964a9ac70b0b9d0b6bc184
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-08-18 20:31:14 +0530

    Created a basic template form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
