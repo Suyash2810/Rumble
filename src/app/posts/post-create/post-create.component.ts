af19f423ca9f0fd14e840f946826d4b2e0423b19
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-07 14:56:22 +0530

    Added image upload functionality

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -20,8 +20,17 @@ export class PostCreateComponent implements OnInit {
 
     this.form = new FormGroup({
       title: new FormControl(null, { validators: [Validators.required] }),
-      content: new FormControl(null, { validators: [Validators.required] })
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] })
     })
+  }
+
+  uploadImage(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    console.log(this.form);
+    console.log(file);
   }
 
   onSubmit() {
