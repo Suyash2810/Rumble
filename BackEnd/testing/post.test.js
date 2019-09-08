4e8d7a36f1506fe370199fa346bd124f850e24f7
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-08 19:25:56 +0530

    Updated the test file for post

--- BackEnd/testing/post.test.js
+++ BackEnd/testing/post.test.js
@@ -13,6 +13,7 @@ describe("Post Model Testing", () => {
       (error) => {
         expect(error.errors.title).to.exist;
         expect(error.errors.content).to.exist;
+        expect(error.errors.imagePath).to.exist;
         done();
       }
     )
@@ -21,11 +22,13 @@ describe("Post Model Testing", () => {
   it("should create the instance with no errors", (done) => {
     let post = new Post({
       title: "foo",
-      content: "foofoo"
+      content: "foofoo",
+      imagePath: "imgPath"
     });
 
     expect(post).to.have.property('title').to.equal("foo");
     expect(post).to.have.property('content').to.equal("foofoo");
+    expect(post).to.have.property('imagePath').to.equal('imgPath');
     done();
   });
 });
