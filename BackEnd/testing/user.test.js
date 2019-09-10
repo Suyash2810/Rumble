5d4a763e57e8840187144f4af0bf46c3c07b2ec3
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-10 19:01:42 +0530

    Created the test file for the user model

---
+++ BackEnd/testing/user.test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const expect = chai.expect;
+const {
+  User
+} = require('../models/user');
+
+describe("Testing User Model", () => {
+
+  it("should create the instance of the User model with errors", (done) => {
+    const user = new User();
+
+    user.validate(
+      (error) => {
+        expect(error.errors.email).to.exist;
+        expect(error.errors.password).to.exist;
+        done();
+      }
+    );
+  });
+
+  it("should create the instance without any errors.", (done) => {
+    const user = new User({
+      email: "foo@gmail.com",
+      password: "foofoo"
+    });
+
+    expect(user).to.have.property('email').to.equal('foo@gmail.com');
+    expect(user).to.have.property('password').to.equal('foofoo');
+    done();
+  });
+});
