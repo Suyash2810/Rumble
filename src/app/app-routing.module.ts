50678b1f59215a631675bad6c9fd02de3dd20463
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-10 18:02:53 +0530

    Updated the sign in form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { PostEditComponent } from "./posts/post-edit/post-edit.component";
+import { SigninComponent } from "./auth/signin/signin.component";
 
 const appRoutes: Routes = [
     { path: '', component: PostListComponent },
     { path: 'create', component: PostCreateComponent },
-    { path: 'edit/:postID', component: PostEditComponent }
+    { path: 'edit/:postID', component: PostEditComponent },
+    { path: 'login', component: SigninComponent }
 ];
 
 @NgModule({
