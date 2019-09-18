efc69f5bf9e6adf18d0b4f4b8983eaaee41ffc19
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-18 23:33:33 +0530

    Updated the test files.

--- BackEnd/testing/post.test.js
+++ BackEnd/testing/post.test.js
@@ -11,9 +11,12 @@ describe("Post Model Testing", () => {
 
     post.validate(
       (error) => {
+        expect(error.errors.username).to.exist;
         expect(error.errors.title).to.exist;
+        expect(error.errors.description).to.exist;
         expect(error.errors.content).to.exist;
         expect(error.errors.imagePath).to.exist;
+        expect(error.errors.creator_id).to.exist;
         done();
       }
     )
@@ -21,14 +24,20 @@ describe("Post Model Testing", () => {
 
   it("should create the instance with no errors", (done) => {
     let post = new Post({
+      username: 'fooo',
       title: "foo",
+      description: 'fooing',
       content: "foofoo",
-      imagePath: "imgPath"
+      imagePath: "imgPath",
+      creator_id: 'sdbfust38274t32gvsddfyt'
     });
 
+    expect(post).to.have.property('username').to.equal('fooo');
     expect(post).to.have.property('title').to.equal("foo");
+    expect(post).to.have.property('description').to.equal('fooing');
     expect(post).to.have.property('content').to.equal("foofoo");
     expect(post).to.have.property('imagePath').to.equal('imgPath');
+    expect(post).to.have.property('creator_id').to.equal('sdbfust38274t32gvsddfyt');
     done();
   });
 });
--- BackEnd/testing/user.test.js
+++ BackEnd/testing/user.test.js
@@ -13,6 +13,7 @@ describe("Testing User Model", () => {
       (error) => {
         expect(error.errors.email).to.exist;
         expect(error.errors.password).to.exist;
+        expect(error.errors.username).to.exist;
         done();
       }
     );
@@ -20,10 +21,12 @@ describe("Testing User Model", () => {
 
   it("should create the instance without any errors.", (done) => {
     const user = new User({
+      username: "foo",
       email: "foo@gmail.com",
       password: "foofoo"
     });
 
+    expect(user).to.have.property('username').to.equal('foo');
     expect(user).to.have.property('email').to.equal('foo@gmail.com');
     expect(user).to.have.property('password').to.equal('foofoo');
     done();
