1ef74ba80fc124b952a5f333061ee80621a3d135
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-21 19:02:56 +0530

    Added http requests to the comment service.

--- src/app/posts/post-view/post-comment/comment.service.ts
+++ src/app/posts/post-view/post-comment/comment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Comment, CommentModel } from './comment.model';
+import { map } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
 
@@ -13,7 +14,77 @@ export class CommentService {
         let data: Comment = new CommentModel(username, imagePath, content, creator_id);
         type responseType = { status: string, comment: any };
 
-        // this.httpClient.post<responseType>('http://localhost:3000/comment', data)
-        console.log(data);
+        this.httpClient.post<responseType>('http://localhost:3000/comment', data)
+            .pipe(
+                map(
+                    (data) => {
+                        return {
+                            id: data.comment._id,
+                            username: data.comment.username,
+                            imagePath: data.comment.imagePath,
+                            content: data.comment.content,
+                            createdAt: data.comment.createdAt,
+                            creator_id: data.comment.creator_id
+                        }
+                    }
+                )
+            )
+            .subscribe(
+                (transformedComment) => {
+                    console.log(transformedComment);
+                }
+            );
+    }
+
+    getComments() {
+        type responseType = { status: string, comments: any };
+        this.httpClient.get<responseType>('http://localhost:3000/comment')
+            .pipe(
+                map(
+                    (data) => {
+                        return data.comments.map(
+                            (comment) => {
+                                return {
+                                    id: comment._id,
+                                    username: comment.username,
+                                    imagePath: comment.imagePath,
+                                    content: comment.content,
+                                    createdAt: comment.createdAt,
+                                    creator_id: comment.creator_id
+                                }
+                            }
+                        )
+                    }
+                )
+            )
+            .subscribe(
+                (transformedCommentData) => {
+                    console.log(transformedCommentData);
+                }
+            )
+    }
+
+    getCommentById(id: string) {
+        type responseType = { status: string, comment: any };
+        this.httpClient.get<responseType>('http://localhost:3000/comment/' + id)
+            .pipe(
+                map(
+                    (data) => {
+                        return {
+                            id: data.comment._id,
+                            username: data.comment.username,
+                            imagePath: data.comment.imagePath,
+                            content: data.comment.content,
+                            createdAt: data.comment.createdAt,
+                            creator_id: data.comment.creator_id
+                        }
+                    }
+                )
+            )
+            .subscribe(
+                (transformedComment) => {
+                    console.log(transformedComment);
+                }
+            )
     }
 }
