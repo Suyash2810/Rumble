50cb3e43546a1238ed13804b6d582c455e01641d
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-21 13:53:26 +0530

    Created the comment model.

---
+++ BackEnd/models/comments.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+
+var commentSchema = new mongoose.Schema({
+  username: {
+    required: true,
+    type: String
+  },
+  imagePath: {
+    required: true,
+    type: String
+  },
+  createdAt: {
+    type: String,
+    default: moment().format("MMM Do YY")
+  },
+  content: {
+    required: true,
+    type: String
+  },
+  creator_id: {
+    type: String,
+    required: true
+  }
+});
+
+var Comment = mongoose.model("Comment", commentSchema);
+
+exports = {
+  Comment
+}
