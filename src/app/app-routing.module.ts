970acf2be8f5c9f8c09f4cee26238fc13919956d
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-22 19:37:46 +0530

    Set up routing of home page components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,18 @@ import { SignupComponent } from "./auth/signup/signup.component";
 import { AuthGuard } from "./auth/auth.guard";
 import { PostViewComponent } from "./posts/post-view/post-view.component";
 import { HomeComponent } from "./core/home/home.component";
+import { BasicComponent } from "./core/home/basic/basic.component";
+import { ProfileComponent } from "./core/home/profile/profile.component";
+import { ContactComponent } from "./core/home/contact/contact.component";
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
+    {
+        path: '', component: HomeComponent, children: [
+            { path: '', component: BasicComponent },
+            { path: 'profile', component: ProfileComponent },
+            { path: 'contact', component: ContactComponent }
+        ]
+    },
     { path: 'list', component: PostListComponent },
     { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
     { path: 'edit/:postID', component: PostEditComponent, canActivate: [AuthGuard] },
