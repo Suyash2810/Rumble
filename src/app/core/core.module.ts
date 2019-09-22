40762a6b66ff3a780533ba988ea3b2620cd5e38d
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-22 19:18:53 +0530

    Created the base component for the home page.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { NgModule } from "@angular/core";
 import { MaterialModule } from "../material.module";
+import { BasicComponent } from './home/basic/basic.component';
 
 @NgModule({
     declarations: [
         HeaderComponent,
-        HomeComponent
+        HomeComponent,
+        BasicComponent
     ],
     imports: [
         SharedModule,
