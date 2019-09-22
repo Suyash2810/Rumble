8859bb836764f21177236e189d231cdcaf89ae04
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-22 19:25:47 +0530

    Created the contact component for the main page.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,13 +6,15 @@ import { NgModule } from "@angular/core";
 import { MaterialModule } from "../material.module";
 import { BasicComponent } from './home/basic/basic.component';
 import { ProfileComponent } from './home/profile/profile.component';
+import { ContactComponent } from './home/contact/contact.component';
 
 @NgModule({
     declarations: [
         HeaderComponent,
         HomeComponent,
         BasicComponent,
-        ProfileComponent
+        ProfileComponent,
+        ContactComponent
     ],
     imports: [
         SharedModule,
