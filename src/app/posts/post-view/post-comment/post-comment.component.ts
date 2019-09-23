b794b574908fcad751f18a0689716fc256cdea21
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-23 23:55:48 +0530

    Updated the disable comment function.

--- src/app/posts/post-view/post-comment/post-comment.component.ts
+++ src/app/posts/post-view/post-comment/post-comment.component.ts
@@ -12,6 +12,8 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { CommentService } from './comment.service';
 import { Comment } from './comment.model';
 import { Subscription } from 'rxjs';
+import { Params, ActivatedRoute } from '@angular/router';
+import { PostService } from '../../posts.service';
 
 @Component({
   selector: 'app-post-comment',
@@ -29,6 +31,8 @@ export class PostCommentComponent implements OnInit, OnDestroy {
   commentSubs: Subscription;
   isLoading: boolean = true;
   checked: boolean = false;
+  postID: string = " ";
+  postCreatorId: string = " ";
 
   public tools: object = {
     items: ['Undo', 'Redo', '|',
@@ -46,7 +50,7 @@ export class PostCommentComponent implements OnInit, OnDestroy {
       'Replace', 'Align', 'Caption', 'Remove', 'InsertLink', '-', 'Display', 'AltText', 'Dimension']
   };
 
-  constructor(private authService: AuthService, private commentService: CommentService) { }
+  constructor(private authService: AuthService, private commentService: CommentService, private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit() {
     this.userID = this.authService.getCurrentUserId();
@@ -59,6 +63,19 @@ export class PostCommentComponent implements OnInit, OnDestroy {
         this.comments = comments;
         console.log(this.comments);
         this.isLoading = false;
+      }
+    );
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.postID = params['postID'];
+      }
+    );
+
+    this.postService.getPost(this.postID);
+    this.postService.getOnePost().subscribe(
+      (post) => {
+        this.postCreatorId = post.creator_id;
       }
     )
   }
