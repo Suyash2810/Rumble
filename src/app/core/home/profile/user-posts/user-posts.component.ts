ea28ad8eea4389a08fb48d23e946aa6ed6820ca2
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-27 02:08:23 +0530

    Updated the user posts component

--- src/app/core/home/profile/user-posts/user-posts.component.ts
+++ src/app/core/home/profile/user-posts/user-posts.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/auth/auth.service';
 import { PostService } from 'src/app/posts/posts.service';
 import { Post } from 'src/app/posts/post.model';
 import { Subscription } from 'rxjs';
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-user-posts',
@@ -13,8 +13,9 @@ export class UserPostsComponent implements OnInit {
 
   posts: Post[] = [];
   postsSub: Subscription;
+  isLoading: boolean = true;
 
-  constructor(private authService: AuthService, private postService: PostService) { }
+  constructor(private postService: PostService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.postService.getPostByCreatorId();
@@ -22,9 +23,13 @@ export class UserPostsComponent implements OnInit {
     this.postsSub = this.postService.getUsersPostListener().subscribe(
       (posts) => {
         this.posts = posts;
+        this.isLoading = false;
       }
     )
   }
 
+  navigate(id: string) {
+    this.router.navigate(['/', 'view', id]);
+  }
 
 }
