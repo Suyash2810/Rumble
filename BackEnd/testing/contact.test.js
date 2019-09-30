8a3556a598c5395ffbbcf8abc2e4bb9dea8d1911
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-09-30 21:30:10 +0530

    Updated the test files for the contact model

--- BackEnd/testing/contact.test.js
+++ BackEnd/testing/contact.test.js
@@ -16,6 +16,8 @@ describe("Contact Model", () => {
         expect(error.errors.email).to.exist;
         expect(error.errors.phone).to.exist;
         expect(error.errors.content).to.exist;
+        expect(error.errors.tag).to.exist;
+        expect(error.errors.subject).to.exist;
         done();
       }
     );
@@ -28,7 +30,10 @@ describe("Contact Model", () => {
       email: "foo@foo.com",
       phone: "foofoo",
       content: "fooo",
-      creator_id: "idid"
+      creator_id: "idid",
+      tag: "tag",
+      subject: "fufu",
+      createdAt: "fufufu"
     });
 
     expect(contact).to.have.property('username').to.equal('foo');
@@ -36,6 +41,9 @@ describe("Contact Model", () => {
     expect(contact).to.have.property('phone').to.equal('foofoo');
     expect(contact).to.have.property('content').to.equal('fooo');
     expect(contact).to.have.property('creator_id').to.equal('idid');
+    expect(contact).to.have.property('tag').to.equal('tag');
+    expect(contact).to.have.property('subject').to.equal('fufu');
+    expect(contact).to.have.property('createdAt').to.equal('fufufu');
     done();
   });
 });
