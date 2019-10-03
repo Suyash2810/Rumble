d580b05da94514978e1339673f7a1c888d4bc034
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-03 18:35:17 +0530

    set up inputs to pass data from parent to sub component in comments section.

--- src/app/posts/post-view/post-comment/sub-reply-comments/sub-reply-comments.component.ts
+++ src/app/posts/post-view/post-comment/sub-reply-comments/sub-reply-comments.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, Input, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/auth/user.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sub-reply-comments',
   templateUrl: './sub-reply-comments.component.html',
   styleUrls: ['./sub-reply-comments.component.css']
 })
-export class SubReplyCommentsComponent implements OnInit {
+export class SubReplyCommentsComponent implements OnInit, OnDestroy {
 
   comment: string = " ";
   @ViewChild('f') subform: NgForm;
   @Output() toggler = new EventEmitter<{ status: boolean }>();
+  @Input() parentId: string = " ";
+  @Input() postId: string = " ";
+  userInfo: User;
+  userSub: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getUserInfo();
+    this.userInfo = this.authService.getStaticUserInfo();
+    this.userSub = this.authService.getUserInfoListener().subscribe(
+      (user: User) => {
+        this.userInfo = user;
+      }
+    );
   }
 
   onSubmit() {
@@ -24,5 +38,9 @@ export class SubReplyCommentsComponent implements OnInit {
   closeSubComponent() {
     let toggleData = { status: false };
     this.toggler.emit(toggleData);
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
