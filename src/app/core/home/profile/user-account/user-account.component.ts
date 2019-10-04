cc4cfc4d46abbbc2385be54e35919d114bedaf5a
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-05 00:26:13 +0530

    Improved scollable layout and removed minor errors.

--- src/app/core/home/profile/user-account/user-account.component.ts
+++ src/app/core/home/profile/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-user-account',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccountComponent implements OnInit {
 
-  constructor() { }
+  username: string = " ";
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.username = this.authService.getCurrentUsername();
   }
 
 }
