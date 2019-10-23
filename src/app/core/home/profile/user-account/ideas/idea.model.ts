250595cb32026348b2ef50e85aa8a7d981b48cc8
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-23 20:13:52 +0530

    Created the note model and service.

---
+++ src/app/core/home/profile/user-account/ideas/idea.model.ts
@@ -0,0 +1,8 @@
+export interface Note {
+    id: string;
+    title: string;
+    description: string;
+    userId: string;
+    color: string;
+    createdAt: string;
+}
---
+++ src/app/core/home/profile/user-account/ideas/idea.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from "@angular/core";
+import { MatDialog } from "@angular/material";
+import { Note } from "./idea.model";
+import { Subject } from "rxjs";
+import { AuthService } from "src/app/auth/auth.service";
+import { HttpClient } from "@angular/common/http";
+import { ErrorComponent } from "src/app/error/error.component";
+import { map } from "rxjs/operators";
+
+@Injectable({ providedIn: 'root' })
+
+export class IdeaService {
+
+    private notes: Array<Note> = [];
+    private notesListener = new Subject<Array<Note>>();
+
+    constructor(private dialog: MatDialog, private authService: AuthService, private httpClient: HttpClient) {
+
+    }
+
+    addNote(title: string, description: string, color: string) {
+
+        const userId = this.authService.getCurrentUserId();
+
+        let data = {
+            title,
+            description,
+            color,
+            userId
+        };
+
+        type responseType = { status: string, note: any };
+
+        this.httpClient.post<responseType>("http://localhost:3000/note", data)
+            .subscribe(
+                (data) => {
+                    this.getNotes();
+                },
+                (error) => {
+                    this.dialog.open(ErrorComponent, {
+                        data: {
+                            message: error.error.error.message
+                        }
+                    });
+                }
+            );
+    }
+
+    getNotes() {
+
+        type responseType = { status: string, notes: any };
+
+        this.httpClient.get<responseType>("http://localhost:3000/note")
+            .pipe(
+                map(
+                    (data) => {
+                        return data.notes.map(
+                            (note) => {
+                                return {
+                                    id: note._id,
+                                    title: note.title,
+                                    description: note.description,
+                                    color: note.color,
+                                    userId: note.userId,
+                                    createdAt: note.createdAt
+                                }
+                            }
+                        )
+                    }
+                )
+            )
+            .subscribe(
+                (transformedData: Array<Note>) => {
+                    this.notes = transformedData;
+                    this.notesListener.next([...this.notes]);
+                },
+                (error) => {
+                    this.dialog.open(ErrorComponent, {
+                        data: {
+                            message: error.error.error.message
+                        }
+                    });
+                }
+            );
+    }
+
+    getStaticNotes() {
+        return this.notes;
+    }
+
+    getNotesListener() {
+        return this.notesListener.asObservable();
+    }
+}
