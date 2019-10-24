87c8d7fb49944e9a2ebc18d8f8a5c740d68c2394
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-24 15:18:37 +0530

    Changed the note component layout and created the form.

--- src/app/core/home/profile/user-account/ideas/ideas.component.ts
+++ src/app/core/home/profile/user-account/ideas/ideas.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdeasComponent implements OnInit {
 
+  colors: Array<{ key: string, value: string }> = [
+    {
+      key: 'Caribbean Green',
+      value: '#00BFA5'
+    },
+    {
+      key: 'Vivid Cyan',
+      value: '#00B8D4'
+    },
+    {
+      key: 'Dodger Blue',
+      value: '#0091EA'
+    },
+    {
+      key: 'Mosqe',
+      value: '#006064'
+    },
+    {
+      key: 'Limeade',
+      value: '#01579B'
+    },
+    {
+      key: 'Medium Turquoise',
+      value: '#4DD0E1'
+    },
+    {
+      key: 'Cobalt',
+      value: '#0D47A1'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
