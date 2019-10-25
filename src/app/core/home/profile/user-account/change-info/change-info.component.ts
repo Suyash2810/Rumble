c3129799704144ed3a4361e5417ce6b4db0d194b
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-25 15:10:54 +0530

    Updated the form

--- src/app/core/home/profile/user-account/change-info/change-info.component.ts
+++ src/app/core/home/profile/user-account/change-info/change-info.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+import { User } from 'src/app/auth/user.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-change-info',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeInfoComponent implements OnInit {
 
-  constructor() { }
+  username: string = " ";
+  email: string = " ";
+  pass: string = " ";
+  confPass: string = " ";
+  userInfoSub: Subscription;
+  userInfo: User;
+  isLoading: boolean = true;
+  @ViewChild('f') form: NgForm;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.getUserInfo();
+    this.userInfo = this.authService.getStaticUserInfo();
+    this.userInfoSub = this.authService.getUserInfoListener().subscribe(
+      (user: User) => {
+        this.userInfo = user;
+        this.isLoading = false;
+      }
+    )
+  }
+
+  onSubmit() {
+    console.log(this.form);
   }
 
 }
