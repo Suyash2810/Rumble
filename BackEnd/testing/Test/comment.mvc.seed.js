a6ad83aef0f3cc56124e3ed541b6fce2fc9fe08b
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-28 03:16:06 +0530

    Post test initial.

--- BackEnd/testing/Test/comment.mvc.seed.js
+++ BackEnd/testing/Test/comment.mvc.seed.js
@@ -1,45 +1,45 @@
-const {
-  Comment
-} = require('../../models/comments');
+// const {
+//   Comment
+// } = require('../../models/comments');
 
-const {
-  ObjectID
-} = require('mongodb');
+// const {
+//   ObjectID
+// } = require('mongodb');
 
-let comments = [{
-    _id: new ObjectID().toHexString(),
-    username: "Eliot",
-    imagePath: 'http://localhost:3000/images/userImages/attractive-casual-close-up-1906997.jpg_1569671610409.jpeg',
-    createdAt: 'Sep 28th 19',
-    content: 'This is some random content.',
-    creator_id: new ObjectID().toHexString(),
-    postId: new ObjectID().toHexString()
-  },
-  {
-    _id: new ObjectID().toHexString(),
-    username: "Brandon",
-    imagePath: 'http://localhost:3000/images/userImages/attractive-casual-close-up-1906997.jpg_1569671610409.jpeg',
-    createdAt: 'Sep 28th 19',
-    content: 'This is some random content.',
-    creator_id: new ObjectID().toHexString(),
-    postId: new ObjectID().toHexString()
-  }
-];
+// let comments = [{
+//     _id: new ObjectID().toHexString(),
+//     username: "Eliot",
+//     imagePath: 'http://localhost:3000/images/userImages/attractive-casual-close-up-1906997.jpg_1569671610409.jpeg',
+//     createdAt: 'Sep 28th 19',
+//     content: 'This is some random content.',
+//     creator_id: new ObjectID().toHexString(),
+//     postId: new ObjectID().toHexString()
+//   },
+//   {
+//     _id: new ObjectID().toHexString(),
+//     username: "Brandon",
+//     imagePath: 'http://localhost:3000/images/userImages/attractive-casual-close-up-1906997.jpg_1569671610409.jpeg',
+//     createdAt: 'Sep 28th 19',
+//     content: 'This is some random content.',
+//     creator_id: new ObjectID().toHexString(),
+//     postId: new ObjectID().toHexString()
+//   }
+// ];
 
-const populateComments = (done) => {
+// const populateComments = (done) => {
 
-  Comment.remove({}).then(
-    () => {
-      return Comment.insertMany(comments).then(
-        () => {
-          done();
-        }
-      );
-    }
-  )
-}
+//   Comment.remove({}).then(
+//     () => {
+//       return Comment.insertMany(comments).then(
+//         () => {
+//           done();
+//         }
+//       );
+//     }
+//   )
+// }
 
-module.exports = {
-  populateComments,
-  comments
-}
+// module.exports = {
+//   populateComments,
+//   comments
+// }
