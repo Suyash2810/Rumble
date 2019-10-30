fd28bcc54ce6b71ff447b3466e42025968fe7c95
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-10-31 00:34:38 +0530

    Post test file update 1

--- BackEnd/testing/post.mvc.test.js
+++ BackEnd/testing/post.mvc.test.js
@@ -47,5 +47,32 @@ describe("Post MVC Test", () => {
           }
         );
     });
+
+    it("should get all the posts for invalid query params.", (done) => {
+
+      supertest(app)
+        .get(`/posts`)
+        .expect(200)
+        .expect(
+          (response) => {
+            let data = response.body;
+            expect(data.status).to.exist;
+            expect(data.status).to.be.equal('The data was sent successfully');
+            expect(data.content).to.not.be.null;
+            expect(data.content.length).to.be.equal(posts.length);
+            expect(data.maxPosts).to.be.equal(posts.length);
+          }
+        )
+        .end(
+          (err, result) => {
+            if (err) {
+              return done(err);
+            }
+
+            expect(result.statusCode).to.be.equal(200);
+            done();
+          }
+        );
+    });
   });
 });
