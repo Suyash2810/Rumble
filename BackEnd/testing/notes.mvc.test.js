ba23ad9a3273895ff676eb6a844d5fe6d83d9fd4
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-12-17 16:52:57 +0530

    Note test file update 1

--- BackEnd/testing/notes.mvc.test.js
+++ BackEnd/testing/notes.mvc.test.js
@@ -87,7 +87,91 @@ describe("Note MVC Tests", () => {
         .expect(
           (response) => {
             expect(response.body).to.exist;
-            console.log(response.body);
+            status = response.body.status;
+            expect(status).to.be.equal('The data has been saved successfully.');
+            expect(response.body.note._id).to.exist;
+            expect(response.body.note.title).to.be.equal(data.title);
+            expect(response.body.note.description).to.be.equal(data.description);
+            expect(response.body.note.color).to.be.equal(data.color);
+            expect(response.body.note.userId).to.be.equal(data.userId);
+          }
+
+        )
+        .end(
+          (err, result) => {
+            if (err) {
+              return done(err);
+            }
+
+            expect(result.statusCode).to.be.equal(200);
+            done();
+          }
+        );
+    });
+
+    it("should not save the note for authorized user with invalid data", (done) => {
+
+      supertest(app)
+        .post('/note')
+        .set('authaccess', user1Token)
+        .expect(400)
+        .end(
+          (err, result) => {
+            if (err) {
+              return done(err);
+            }
+
+            expect(result.statusCode).to.be.equal(400);
+            done();
+          }
+        );
+    });
+
+    it("should not save the note for unauthorized user", (done) => {
+
+      let data = {
+        title: "foo",
+        description: "foofoo",
+        color: "fofo",
+        userId: notes[0].userId
+      }
+
+      supertest(app)
+        .post('/note')
+        .send(data)
+        .expect(401)
+        .end(
+          (err, result) => {
+            if (err) {
+              return done(err);
+            }
+
+            expect(result.statusCode).to.be.equal(401);
+            done();
+          }
+        );
+    });
+  });
+
+  context("delete note requests", () => {
+
+    it("should delete the note", (done) => {
+
+      supertest(app)
+        .delete(`/note/${notes[0]._id}`)
+        .set('authaccess', user1Token)
+        .expect(200)
+        .expect(
+          (response) => {
+            expect(response.body).to.exist;
+            expect(response.body.status).to.be.equal('The note has been deleted.');
+            let data = response.body.note;
+            expect(data._id).to.be.equal(notes[0]._id);
+            expect(data.title).to.be.equal(notes[0].title);
+            expect(data.description).to.be.equal(notes[0].description);
+            expect(data.userId).to.be.equal(notes[0].userId);
+            expect(data.createdAt).to.exist;
+            expect(data.color).to.be.equal(notes[0].color);
           }
         )
         .end(
