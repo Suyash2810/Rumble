65fdd6fe25608c5ec10ebce86e6733e0c485810e
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-01-16 15:27:55 +0530

    Created seed file for view posts.

---
+++ BackEnd/testing/Test/view_fav.mvc.seed.js
@@ -0,0 +1,37 @@
+const {
+  PostData
+} = require('../../models/post_data');
+
+const {
+  ObjectID
+} = require('mongodb');
+
+let id1 = new ObjectID().toHexString();
+let id2 = new ObjectID().toHexString();
+
+let viewFavs = [{
+    _id: new ObjectID().toHexString(),
+    parent_post_id: id1
+  },
+  {
+    _id: new ObjectID().toHexString(),
+    parent_post_id: id2
+  }
+];
+
+let populateViewFavs = (done) => {
+
+  PostData.remove({})
+    .then(
+      () => {
+        PostData.insertMany(viewFavs).then(() => done())
+      }
+    );
+}
+
+module.exports = {
+  id1,
+  id2,
+  viewFavs,
+  populateViewFavs
+}
