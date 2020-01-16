dfeec9f53b7927da394c6e33cdc8027aac2a9286
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2020-01-16 11:50:43 +0530

    Created model and service.

---
+++ src/app/posts/post-view/post-view-fav/post-view-fav.model.ts
@@ -0,0 +1,6 @@
+export interface ViewFav {
+
+    views: number;
+    favorites: number;
+    parent_post_id: string;
+}
---
+++ src/app/posts/post-view/post-view-fav/post-view-fav.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { ViewFav } from './post-view-fav.model';
+
+@Injectable({ providedIn: 'root' })
+
+export class ViewFavService {
+
+    constructor() { }
+
+}
